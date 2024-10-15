4b309fd7ab29719a341b832a4d4b713fa222d517
L3025 <L3025@L3025-23>
2024-10-15 13:51:04 +0300

    send this nig

--- BLL/DAL/Product.cs
+++ BLL/DAL/Product.cs
@@ -1,7 +1,18 @@
-﻿namespace BLL.DAL
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DAL
 {
     public class Product
     {
       
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
